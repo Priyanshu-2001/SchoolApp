bb586a34dc0ad428e14b0c7654de066db1857f34
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-09-30 00:03:50 +0530

    student profile section design added with navigation from dashboard

--- app/src/main/java/com/geek/schoolapp/StudentProfile.kt
+++ app/src/main/java/com/geek/schoolapp/StudentProfile.kt
@@ -1,7 +1,32 @@
 package com.geek.schoolapp
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.geek.schoolapp.databinding.AddStudentBinding
 
-class StudentProfile : AppCompatActivity(){
+class StudentProfile : AppCompatActivity() {
+    lateinit var binding: AddStudentBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.add_student)
+
+        if (intent.extras != null) {
+            Log.e("TAG", "onCreate: ")
+            if (intent.extras!!.get("tag")?.equals("Add Student") == true) {
+                binding.tvTitle.text = "Add Student"
+            }
+            if (intent.extras!!.get("tag")?.equals("Delete Student") == true) {
+                binding.tvTitle.text = "Delete Student"
+            }
+            if (intent.extras!!.get("tag")?.equals("Edit Student") == true) {
+                binding.tvTitle.text = "Edit Student"
+            }
+            if (intent.extras!!.get("tag")?.equals("View Student") == true) {
+                binding.tvTitle.text = "View Student"
+            }
+        }
+    }
 }
--- app/src/main/java/com/geek/schoolapp/adapter/dashBoard_adapter.kt
+++ app/src/main/java/com/geek/schoolapp/adapter/dashBoard_adapter.kt
@@ -1,6 +1,7 @@
 package com.geek.schoolapp.adapter
 
-import android.media.Image
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +10,23 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.geek.schoolapp.R
+import com.geek.schoolapp.StudentProfile
 
 class dashBoard_adapter(val data : List<String>) : RecyclerView.Adapter<dashBoard_adapter.dashBoard_viewHolder>() {
-
 
     class dashBoard_viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val  txtView : TextView = itemView.findViewById(R.id.textView)
         val image : ImageView = itemView.findViewById(R.id.image)
         val cardView : CardView = itemView.findViewById(R.id.cardView)
+
+        init {
+            cardView.setOnClickListener{
+                val intent = Intent(it.context,StudentProfile::class.java)
+                intent.putExtra("tag" , txtView.text)
+                Log.e("DashBoard_Adapter", ": ${txtView.text}" )
+                it.context.startActivity(intent)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): dashBoard_viewHolder {
