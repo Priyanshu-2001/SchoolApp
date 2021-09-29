a6d8a95861347adc9af02864cbca538934dadf8b
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-09-29 23:27:24 +0530

    dashboard_added

--- app/src/main/java/com/geek/schoolapp/MainActivity.kt
+++ app/src/main/java/com/geek/schoolapp/MainActivity.kt
@@ -1,14 +1,20 @@
-package com.geek.schoolapp;
+package com.geek.schoolapp
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.geek.schoolapp.R
+import com.geek.schoolapp.databinding.ActivityMainBinding
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this,admin_dashboard::class.java)
+            startActivity(intent)
+        }
     }
 }
