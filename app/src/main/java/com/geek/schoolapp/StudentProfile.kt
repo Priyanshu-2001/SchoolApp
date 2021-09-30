5e297e258df03c34a2ac3475c6c9a6620b2134cd
RishabhRajDhiman <dhimanrishabhraj@gmail.com>
2021-09-30 23:25:56 +0530

    spinners added final

--- app/src/main/java/com/geek/schoolapp/StudentProfile.kt
+++ app/src/main/java/com/geek/schoolapp/StudentProfile.kt
@@ -2,8 +2,13 @@ package com.geek.schoolapp
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.databinding.DataBindingUtil
 import com.geek.schoolapp.databinding.AddStudentBinding
 import com.geek.schoolapp.databinding.ViewDialogBinding
@@ -11,7 +16,6 @@ import com.geek.schoolapp.databinding.ViewDialogBinding
 class StudentProfile : AppCompatActivity() {
     lateinit var binding: AddStudentBinding
     lateinit var whichText :String
-
 
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
