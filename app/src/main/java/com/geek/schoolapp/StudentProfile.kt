c7cf2001b23228b8e82adbf43c5f1885964395fb
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-09-30 12:37:23 +0530

    student profile section design added with navigation from dashboard

--- app/src/main/java/com/geek/schoolapp/StudentProfile.kt
+++ app/src/main/java/com/geek/schoolapp/StudentProfile.kt
@@ -1,7 +1,6 @@
 package com.geek.schoolapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.geek.schoolapp.databinding.AddStudentBinding
@@ -14,19 +13,7 @@ class StudentProfile : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.add_student)
 
         if (intent.extras != null) {
-            Log.e("TAG", "onCreate: ")
-            if (intent.extras!!.get("tag")?.equals("Add Student") == true) {
-                binding.tvTitle.text = "Add Student"
-            }
-            if (intent.extras!!.get("tag")?.equals("Delete Student") == true) {
-                binding.tvTitle.text = "Delete Student"
-            }
-            if (intent.extras!!.get("tag")?.equals("Edit Student") == true) {
-                binding.tvTitle.text = "Edit Student"
-            }
-            if (intent.extras!!.get("tag")?.equals("View Student") == true) {
-                binding.tvTitle.text = "View Student"
-            }
+            binding.tvTitle.text = (intent.extras!!.get("tag")).toString()
         }
     }
 }
