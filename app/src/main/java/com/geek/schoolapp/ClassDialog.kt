ec99176840610da51e78bc2f35ea01814bc83d67
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-10-03 22:07:25 +0530

    *temporary push added*
    as api is not working till now
    "login" api
    
    Api validation yet to be done (Showing server down )

--- app/src/main/java/com/geek/schoolapp/ClassDialog.kt
+++ app/src/main/java/com/geek/schoolapp/ClassDialog.kt
@@ -3,11 +3,14 @@ package com.geek.schoolapp
 import android.app.ActionBar
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams.FLAG_DIM_BEHIND
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import com.geek.schoolapp.databinding.ViewDialogBinding
 
@@ -27,7 +30,16 @@ class ClassDialog : AppCompatActivity(){
         }.show()
 
         dialogBinding?.btnOk?.setOnClickListener {
-            customDialog.dismiss()
+            val classList = context.resources.getStringArray(R.array.Class)
+            if(dialogBinding.classSpinner.selectedItemPosition!=0){
+                val selectedClass = classList[dialogBinding.classSpinner.selectedItemPosition]
+                val i = Intent(context,studentList::class.java)
+                i.putExtra("class", selectedClass)
+                it.context.startActivity(i)
+                customDialog.dismiss()
+            }else{
+                Toast.makeText(context, "HEY Select One of the class !!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
