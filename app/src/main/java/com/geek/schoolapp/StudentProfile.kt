f8c148d890d4bb054067648678d5205af0600c99
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-10-03 22:53:24 +0530

    *temporary push added*
    as api is not working till now
    "login" api
    
    Api validation yet to be done (Showing server down )

--- app/src/main/java/com/geek/schoolapp/StudentProfile.kt
+++ app/src/main/java/com/geek/schoolapp/StudentProfile.kt
@@ -2,6 +2,7 @@ package com.geek.schoolapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.geek.schoolapp.dataModel.studentData
@@ -10,30 +11,44 @@ import com.geek.schoolapp.service.addStudentService
 
 class StudentProfile : AppCompatActivity() {
     lateinit var binding: AddStudentBinding
-    lateinit var whichText :String
+    lateinit var whichText: String
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.add_student)
 
         if (intent.extras != null) {
             binding.tvTitle.text = (intent.extras!!.get("tag")).toString()
-           whichText = (intent.extras!!.get("tag")).toString()
+            whichText = (intent.extras!!.get("tag")).toString()
         }
-            if(whichText == "Add Student"){
-                addStudentConfig()
-            }
+        if (whichText == "Add Student") {
+            addStudentConfig()
+        }
     }
 
     private fun addStudentConfig() {
         binding.userNameBox.visibility = View.VISIBLE
         binding.passwordBox.visibility = View.VISIBLE
-        val data = studentData(binding.nameField.text.toString() , binding.rollfield.text.toString() ,"class" ,binding.regField.text.toString() ,binding.fnameField.text.toString())
-        data.passWord = binding.password.text.toString()
-        data.userName = binding.username.text.toString()
+
         val service = addStudentService()
+
         binding.btnSave.setOnClickListener {
-            service.addStudent(data,this)
+            val classSelected = binding.classSpinner.selectedItemPosition
+            if (classSelected != 0) {
+                val data = studentData(
+                    binding.nameField.text.toString(),
+                    Integer.valueOf(binding.rollfield.text.toString()),
+                    classSelected,
+                    binding.regField.text.toString(),
+                    binding.fnameField.text.toString()
+                )
+                data.passWord = binding.password.text.toString()
+                data.userName = binding.username.text.toString()
+
+                service.addStudent(data, this)
+            }else{
+                Toast.makeText(it.context, "Hey You Forgot to Select the class ", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/geek/schoolapp/service/addStudentService.kt
+++ app/src/main/java/com/geek/schoolapp/service/addStudentService.kt
@@ -1,42 +1,63 @@
 package com.geek.schoolapp.service
 
 import android.content.Context
-import android.content.SharedPreferences
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
 import com.android.volley.AuthFailureError
-import com.android.volley.Header
-import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.geek.schoolapp.dataModel.studentData
 import org.json.JSONObject
-import java.lang.reflect.Method
-import java.util.HashMap
+import java.util.*
 
 class addStudentService() {
 
-    fun addStudent(data: studentData , context : Context) {
+    fun addStudent(data: studentData, context: Context) {
         val requestQueue = Volley.newRequestQueue(context)
         val obj = JSONObject()
-        val standard : Int = data.standard as Int
-        val roll : Int = data.rollNO as Int
-        obj.put("username",data.userName)
-        obj.put("password",data.passWord)
-        obj.put("registeration_id",data.registrationNo)
-        obj.put("standard",standard)
-        obj.put("father_name",data.fatherName)
-        obj.put("name",data.name)
-        obj.put("roll_number",roll)
+        val standard: Int = data.standard as Int
+        val roll: Int = data.rollNO
+        obj.put("username", data.userName)
+        obj.put("password", data.passWord)
+        obj.put("registeration_id", data.registrationNo)
+        obj.put("standard", standard)
+        obj.put("father_name", data.fatherName)
+        obj.put("name", data.name)
+        obj.put("roll_number", roll)
         val URL = "https://schoolapp1121.herokuapp.com/api/student"
-        val request = JsonObjectRequest(Request.Method.POST,URL , obj ,
+
+        val request = object : JsonObjectRequest(1, URL, obj,
             {
-                Toast.makeText(context, "Hey Registration of ${data.name} is successful ", Toast.LENGTH_SHORT).show()
-        },
+                Toast.makeText(
+                    context,
+                    "Hey Registration of ${data.name} is successful ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            },
             {
-                Toast.makeText(context, "Hey Registration of ${data.name} is Failed \n Please Retry after Sometime", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    context,
+                    "Hey Registration of ${data.name} is Failed \n Please Retry after Sometime",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e("TAG", "addStudent: $it")
             }
-        )
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String?> {
+                val params: MutableMap<String, String> = HashMap()
+                val creds = String.format(
+                    "%s:%s",
+                    "admin123",
+                    "123"
+                )
+                val auth = "Basic " + Base64.encodeToString(creds.toByteArray(), Base64.DEFAULT)
+                params["Authorization"] = auth
+                return params
+
+            }
+        }
         requestQueue.add(request)
     }
 }
